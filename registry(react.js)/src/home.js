1a66953f1f535c5ae46515cfb91fb7bc5e8d9551
Bigandy4climate <samuelnihoul@hotmail.fr>
2021-10-16 10:06:44 +0200

    Revert "improvemnets"
    This reverts commit c5422ca2ff82f10e60f663c9a5c0abf81ca519c8.

--- registry(react.js)/src/home.js
+++ registry(react.js)/src/home.js
@@ -2,15 +2,8 @@ import DenseTable from "./components/DenseTable";
 
 export default function Home() {
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Welcome to the Home of CO2 Offsets.</h1>
-      <h2>This is our awesome candy machine.</h2>
-      <p>The more offsets you buy, the rarer the NFT. Good luck! ☘️</p>
-      <p>
-        By the way, each projects features its own natural wonder, so choose
-        wisely!
-      </p>
-      <p>🌺🐴🦕🐙🦐🐣🐷🐮🦁🐡🌴🌺</p>
+    <div>
+      <h1>Welcome to the Home of CO2 offsets.</h1>
       <DenseTable />
     </div>
   );
