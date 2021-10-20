04e53810eda86af717f8525eddb0148ff8da30f0
Marc-Henry Nanguy <marchenrynanguy1@gmail.com>
2021-10-20 14:46:57 +0000

    ok

--- app/src/components/card/Project.vue
+++ app/src/components/card/Project.vue
@@ -42,7 +42,7 @@
           <div class="my-4">
             <q-input v-model="buyerPk" label="Votre adresse public" />
           </div>
-          <q-btn @click="buy" color="black" class="full-width"
+          <q-btn @click="localBuy" color="black" class="full-width"
             >Lancer la transaction</q-btn
           >
         </q-popup-edit>
@@ -60,43 +60,9 @@
 </template>
 
 <script lang="ts">
-import { IProject, IBuyDto } from "@/IProject";
+import { IProject } from "@/IProject";
 import { defineComponent, PropType, ref } from "vue";
-import { useWallet } from "@solana/wallet-adapter-vue";
-// import { AxiosInstance } from "axios";
-import {
-  PublicKey,
-  clusterApiUrl,
-  Connection,
-  SystemProgram,
-} from "@solana/web3.js";
-import {
-  BN,
-  Program,
-  Provider,
-  web3,
-  // setProvider,
-} from "@project-serum/anchor";
-
-// @ts-ignore
-import idl from "../../idls/idl.json";
-
-// @ts-ignore
-import idl2 from "../../idls/idl2.json";
-
-import {
-  getCandyMachine,
-  getMasterEditionAddress,
-  getMetadataAddress,
-  TOKEN_METADATA_PROGRAM_ID,
-} from "./../../helper";
-import {
-  ASSOCIATED_TOKEN_PROGRAM_ID,
-  TOKEN_PROGRAM_ID,
-} from "@solana/spl-token";
-// import { BuyDto } from './mint.dto';
-import { token } from "@project-serum/anchor/dist/cjs/utils";
-import { Wallet } from "@project-serum/anchor/dist/cjs/provider";
+import useSolana from "@/composables/useSolana";
 
 export default defineComponent({
   props: {
@@ -114,141 +80,17 @@ export default defineComponent({
     },
   },
   setup() {
-    const programID = new PublicKey(idl2.metadata.address);
-    const programID2 = new PublicKey(idl.metadata.address);
-    const candyMachineUuid = "GMxBmP";
-    const config = new web3.PublicKey(
-      "GMxBmPkJsAvC4QJXjroagjBQQmSwdC1qhQhaVGL6cjgB"
-    );
-
-    const mint = web3.Keypair.generate();
     // const axios = inject("axios") as AxiosInstance;
 
+    const { buy } = useSolana();
     const label = ref("Acheter");
-    const buyerPk = ref(null);
+    const buyerPk = ref("");
     const value = ref(1);
 
-    async function getProvider(wallet: Wallet) {
-      // const { Keypair } = web3;
-      // const baseAccount = Keypair.generate();
-      const network = clusterApiUrl("devnet");
-
-      // @ts-ignore
-      const connection = new Connection(network, "processed");
-
-      const provider = new Provider(connection, wallet, {
-        preflightCommitment: "processed",
-      });
-
-      return provider;
-    }
-
-    async function buyAndMint({ buyerPk, offsets, wallet }: IBuyDto) {
-      const provider = await getProvider(wallet);
-
-      const sellerAccount = new web3.PublicKey(
-        "E62W9WK5XR6VM9HYMxYyS6gkLLmBiNeBbsFjvBVfY766"
-      );
-      const projectAccount = new web3.PublicKey(
-        "C4WKtnm7mrvftQ748Nm1k8DMV5BPq1EF2Bd1RgZwVTMb"
-      );
-
-      // @ts-ignore
-      const harmoniaProgram = new Program(idl2, programID, provider);
-
-      // @ts-ignore
-      const candyProgram = new Program(idl, programID2, provider);
-
-      const candyProgramId = candyProgram.programId;
-
-      //@ts-ignore
-
-      const metadata = await getMetadataAddress(mint.publicKey);
-      const masterEdition = await getMasterEditionAddress(mint.publicKey);
-
-      const [candyMachine] = await getCandyMachine(
-        config,
-        candyMachineUuid,
-        candyProgramId
-      );
-
-      const tx = await harmoniaProgram.rpc.buyAndMint(new BN(offsets), {
-        accounts: {
-          project: projectAccount,
-          buyer: buyerPk,
-          seller: sellerAccount,
-          candyProgram: candyProgram.programId,
-          config: config,
-          candyMachine: candyMachine,
-          payer: buyerPk,
-          wallet: sellerAccount, // treasury
-          mint: mint.publicKey,
-          associatedToken: token,
-          metadata: metadata,
-          masterEdition: masterEdition,
-          tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
-          ataProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
-          tokenProgram: TOKEN_PROGRAM_ID,
-          systemProgram: SystemProgram.programId,
-          rent: web3.SYSVAR_RENT_PUBKEY,
-          clock: web3.SYSVAR_CLOCK_PUBKEY,
-        },
-        signers: [mint],
-      });
-
-      return tx;
-    }
-
-    async function buy() {
-      const {
-        sendTransaction,
-        signTransaction,
-        signAllTransactions,
-        signMessage,
-        publicKey,
-      } = useWallet();
-
-      const providerWallet = {
-        sendTransaction,
-        get signTransaction() {
-          return signTransaction.value;
-        },
-        get signAllTransactions() {
-          return signAllTransactions.value;
-        },
-        get signMessage() {
-          return signMessage.value;
-        },
-        get publicKey() {
-          return publicKey.value;
-        },
-      };
-
-      await buyAndMint({
-        buyerPk: buyerPk.value ?? publicKey.value!.toBase58(),
-        offsets: value.value,
-        wallet: providerWallet,
-      });
-
-      // // console.log(theWallet.wallet);
-
-      // console.log("ii", providerWallet);
-
-      // const dto: IBuyDto = {
-      //   offsets: value.value,
-      //   buyerPk: buyerPk.value,
-      //   wallet: JSON.stringify(providerWallet),
-      // };
-
-      //   try {
-      //     const { data } = await axios.post("/buy", dto);
-      //     console.log("success", data);
-      //   } catch (error) {
-      //     console.log(error);
-      //   }
-    }
-
-    return { value, buy, label, buyerPk };
+    const localBuy = () => {
+      buy(buyerPk.value, value.value);
+    };
+    return { value, label, buyerPk, localBuy };
   },
 });
 </script>
