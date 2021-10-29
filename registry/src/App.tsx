2a4aa7d6f7292ed6e3431375ec99bbcf2eef7a69
Bigandy4climate <samuelnihoul@hotmail.fr>
2021-10-29 17:57:11 +0200

    Carousel component

--- registry/src/App.tsx
+++ registry/src/App.tsx
@@ -84,7 +84,12 @@ const App = () => {
     []
   );
 const wallet=useWallet()
-  return (
+  return (<>
+    <head>
+      <meta charSet="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/>
+      </head>
       <ThemeProvider theme={theme}>
         <ConnectionProvider endpoint={endpoint}>
           <WalletProvider wallets={wallets} autoConnect={true}>
@@ -102,7 +107,8 @@ const wallet=useWallet()
             </WalletDialogProvider>
           </WalletProvider>
         </ConnectionProvider>
-      </ThemeProvider>
+      </ThemeProvider><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
+</>
   );
 };
 
