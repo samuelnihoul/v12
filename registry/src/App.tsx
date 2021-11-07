1c27b2d50f3c640e0fde473bedc1f85bbb50efa4
Bigandy4climate <samuelnihoul@hotmail.fr>
2021-11-07 17:40:36 +0100

    correctly compiling, unclear where the pb comes from

--- registry/src/App.tsx
+++ registry/src/App.tsx
@@ -23,9 +23,8 @@ import { NewProject } from "./pages/NewProject";
 import React from "react"
 import Signup from "./components/Signup"
 import { Container } from "react-bootstrap"
-import { AuthProvider } from "./context/AuthContext"
+import {AuthProvider}from "./contexts/AuthContext"
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
-import Dashboard from "./components/Dashboard"
 import Login from "./components/Login"
 import PrivateRoute from "./components/PrivateRoute"
 import ForgotPassword from "./components/ForgotPassword"
@@ -65,46 +64,33 @@ const App = () => {
     ],
     []
   );
-const wallet=useAnchorWallet()
-  return (<>
-    
-      
-        <ConnectionProvider endpoint={endpoint}>
-          <WalletProvider wallets={wallets} autoConnect={true}>
-            <WalletDialogProvider>
-            <AuthProvider>
-             <Router>
-               <Switch>
-             <PrivateRoute exact path="/" component={Dashboard} />
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/forgot-password" component={ForgotPassword} />
-               <Route exact path="/" component={()=><Home/>} />  
-               <Route path="/submitAProject" component={
-                 ()=><NewProject wallet={
-                   wallet 
-                  }connection={connection}/>
-                   }
-                   />
-                 
-                   
-                 
-              
-               <Route path="/registry" component={()=><Registry candyMachineId={candyMachineId}
-               config ={config} connection={connection} startDate={startDateSeed} treasury={treasury} 
-                txTimeout={txTimeout}
-               />} />
-               <Route path="/signup" component={()=><Signup/>}/>
-               <Route path="/login" component={()=><Login/>}/>
-               </Switch>
-               </Router>   
-               </AuthProvider>
-            </WalletDialogProvider>
-          </WalletProvider>
-        </ConnectionProvider>
-      
-</>
+  const wallet=useAnchorWallet()
+  return (
+    <>     
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect={true}>
+          <WalletDialogProvider>
+            <Container className="d-flex align-items-center justify-content-center" style={{minHeight: "100vh" }}>
+              <div className="w-100" style={{maxWidth: "400px"  }}>
+                <Router>
+                  <AuthProvider>
+                    <Switch>
+                      <PrivateRoute exact path="/" component={()=><Home/>} />  
+                      <Route path="/submitAProject" component={()=><NewProject wallet={wallet }connection={connection}/>}/>
+                      <Route path="/registry" component={()=><Registry candyMachineId={candyMachineId}config ={config} connection={connection} startDate={startDateSeed} treasury={treasury}txTimeout={txTimeout}/>} />
+                      <PrivateRoute path="/update-profile" component={UpdateProfile} />
+                      <Route path="/signup" component={Signup} />
+                      <Route path="/login" component={Login} />
+                      <Route path="/forgot-password" component={ForgotPassword} />
+                    </Switch>
+                  </AuthProvider>
+                </Router>
+              </div>
+            </Container>
+          </WalletDialogProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </>
   );
 };
 
