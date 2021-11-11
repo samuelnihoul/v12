04ab704c7f0b364e4bc8372ecbddbc8d0ebb71ac
Bigandy4climate <samuelnihoul@hotmail.fr>
2021-11-11 10:10:37 +0100

    line break in about us

--- registry/src/pages/AboutUs.tsx
+++ registry/src/pages/AboutUs.tsx
@@ -8,7 +8,7 @@ export default function AboutUs() {
             <h1>About Us</h1>
             <div >
                 <div>
-                    <h2>We work hard to soothe our planet's fever with quality and affordable offsets. We designed an hybrid CO2 offset marketplace with both fiat and blockchain integration, enabling optimal user experience and overall reliability</h2>
+                    <h2>We work hard to soothe our planet's fever with quality and affordable offsets.<br/>We designed an hybrid CO2 offset marketplace with both fiat and blockchain integration, enabling optimal user experience and overall reliability</h2>
                 </div>
             <div>We see<strong>exotic CO2e offsets.</strong>
                 <br/>We see their potential.<br/><br/>To succeed in the climate fight we must <strong>widen our range of action.<br/><br/>This is what we do.</strong><br/><br/>As a bonus, we aim to provide <strong>seamless blockchain integration</strong> to enhance <strong>safety, reliability and scalability</strong>;<br/><br/>And superior customer service 💪.</div>
