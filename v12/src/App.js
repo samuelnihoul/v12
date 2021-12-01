111719a2f42bc1efdac6dfcb2072ddb55110f374
Nuhutuh25 <73873324+Bigandy4climate@users.noreply.github.com>
2021-12-01 17:20:21 +0100

    fix useless folders

--- v12/src/App.js
+++ v12/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './App.css'
 
 // Home pages
 import CreativeAgency from "./pages/home/CreativeAgency";
