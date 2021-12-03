ca96beb25248e869cb48344978c30fc5cfa4b00f
Nuhutuh25 <73873324+Bigandy4climate@users.noreply.github.com>
2021-12-03 16:09:17 +0100

    fs tree

---
+++ v12/pages/_app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../assets/css/style.css'
+import Demo from './Demos'
+function App() {
+
+  return (<Demo></Demo>
+
+  );
+}
+
+export default App;
---
+++ v12/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./_app";
+const index = () => {
+    return (
+        <App />
+    )
+}
+export default index
