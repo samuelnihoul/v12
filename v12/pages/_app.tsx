94e26846e41ba490a6e809117c45f2d51360fd16
Nuhutuh25 <73873324+Bigandy4climate@users.noreply.github.com>
2021-12-08 19:14:36 +0100

    reput the vids in images

--- v12/pages/_app.tsx
+++ v12/pages/_app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '/assets/css/style.css'
+import '../css/style.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 export default function App({ Component, pageProps }) {
   return <Component className="App" {...pageProps} />
