54ccd7d2ac553ee2f589dd07457ccf535c884c69
Samuel Nihoul <snolly@protonmail.com>
2022-01-14 21:21:11 +0100

    rm shop folders

--- components/Shop/ShopMasonryWrapper.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const ShopMasonryWrapper = ({ layout, children }) =>
-  layout === "masonry" ? <div id="portfolioMasonry">{children}</div> : children;
-
-export default ShopMasonryWrapper;
