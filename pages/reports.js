ec8959da6bcbff4ba9ca988f110e46f0ab22492e
Samuel Nihoul <snolly@protonmail.com>
2022-01-26 10:35:46 +0100

    reports page

---
+++ pages/reports.js
@@ -0,0 +1,18 @@
+import react from "react";
+import bg from "../public/assets/images/reports.jpeg";
+export default function reports() {
+  const img = bg;
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        textColor: "white",
+        backgroundImage: "/assets/images/reports.jpeg",
+        backgroundColor: "lightgray",
+        height: "90vh",
+      }}
+    >
+      <h1 style={{ padding: "7vh" }}>No project reports to show yet</h1>
+    </div>
+  );
+}
