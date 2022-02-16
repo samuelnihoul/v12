735a8e21813090554a094f8f9f78e69fc6d88d00
Samuel Nihoul <snolly@protonmail.com>
2022-02-16 20:52:24 +0100

    better taglines

--- components/Hero/HeroVideo.tsx
+++ components/Hero/HeroVideo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import useWindowSize from "../../helpers/GetWindowSize";
 import SliderButtons from "../../elements/SliderButtons/SliderButtons";
+import { fontSize } from "@mui/system";
 
 const HeroVideo = ({ data }) => {
   const size = useWindowSize();
@@ -10,11 +11,11 @@ const HeroVideo = ({ data }) => {
       <div className="hero-text-wrap overlay-bg">
         <div className="hero-text white-color">
           <div className="container text-center">
-            <h2 className="white-color text-uppercase font-400 letter-spacing-5">
+            <h2 className="white-color font-400 letter-spacing-5" style={{ fontSize:40}}>
               {data.tagline}
             </h2>
-            <h1 className="white-color text-uppercase font-700" style={{background:'linear-gradient(to right, #194d38,#ffd700)', WebkitBackgroundClip:'text', WebkitTextFillColor:'transparent'}}>{data.title}</h1>
-            <h3 className="white-color font-400 text-uppercase" >{data.text}<br></br><span style={{background:'linear-gradient(to right, #9140BF,#ffd700)', WebkitBackgroundClip:'text',WebkitTextFillColor:'transparent'}}>together.</span></h3>
+            <h1>{/* <span style={{fontSize:30}}>The new era</span><br/> */}<span  className="white-color font-700" style={{background:'linear-gradient(to right, #194d38,#ffd700)', WebkitBackgroundClip:'text', WebkitTextFillColor:'transparent'}}>{data.title}</span></h1>
+            <h3 className="white-color font-400 " >{data.text}<br></br><span style={{background:'linear-gradient(to right, #9140BF,#ffd700)', WebkitBackgroundClip:'text',WebkitTextFillColor:'transparent'}}>Together.</span></h3>
             <p className="text-center mt-30">
               <SliderButtons buttons={data.buttons} />
             </p>
