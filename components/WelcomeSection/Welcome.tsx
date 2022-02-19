fc7b3062333424a5cb9cf00db0eb0897008e65b2
bigandy5climate <snolly@protonmail.com>
2022-02-19 16:53:12 +0200

    nfts and hero v

--- components/WelcomeSection/Welcome.tsx
+++ components/WelcomeSection/Welcome.tsx
@@ -3,7 +3,12 @@ import { JsxChild } from 'typescript';
 import HeadingSection from '../HeadingSection/HeadingSection';
 
 const Welcome = ({ title, tagline, children }) => (
+  
   <section className="first-ico-box" id="about">
+    <h6 style={{textAlign: "center"}}>NFT Collection Q1 2022</h6>
+     <img  src={"/assets/images/bitmap.png"}alt="NFTs">
+              </img>
+    
     <div className="container">
       <div className="row">
         <HeadingSection title={title} tagline={tagline} classAppend={null} font={null}>
