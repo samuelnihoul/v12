de176fe922453864fbcfb6cfe30df7305c8a059e
Samuel Nihoul <snolly@protonmail.com>
2022-04-05 20:49:45 +0200

    imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import '../public/icofont/icofont.min.css'
 import HeaderOne from "../components/Header/HeaderOne";
 import FooterOne from "../components/Footer/FooterOne";
+import Sound from 'react-sound'
+
 
 export default function App({ Component, pageProps }) {
  
