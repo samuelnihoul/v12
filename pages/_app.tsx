4afbcc4caa83ec466eb404c1acaf5a6766b6017a
alagroun <102326229+alagroun@users.noreply.github.com>
2022-04-05 17:56:38 +0200

    Update _app.tsx
    Test react_GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import '../public/icofont/icofont.min.css'
 import HeaderOne from "../components/Header/HeaderOne";
 import FooterOne from "../components/Footer/FooterOne";
+import ReactGA from "react-ga";
+
 
 export default function App({ Component, pageProps }) {
- 
-  return (
-  <Component className="App" {...pageProps} />)
+  ReactGA.initialize("G-RFBEGZHSQT");
+  ReactGA.pageview(window.location.pathname + window.location.search);
+  return (<>
+  <Component className="App" {...pageProps} /></>)
 }
