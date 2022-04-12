1f1c19812ecf311eaf060ad7045551eed57af16e
Samuel Nihoul <snolly@protonmail.com>
2022-04-12 22:59:54 +0200

    aligned buttons

--- components/Navs/CartSearchTop.tsx
+++ components/Navs/CartSearchTop.tsx
@@ -3,10 +3,10 @@ import CartMenu from "../../elements/CartMenu";
 import SearchItem from "../../elements/SearchItem";
 
 const CartSearchTop = ({ showSearchForm }) => (
-  <ul>
+  <>
     {/* <CartMenu /> */}
     <SearchItem showSearchForm={showSearchForm} />
-  </ul>
+  </>
 );
 
 export default CartSearchTop;
