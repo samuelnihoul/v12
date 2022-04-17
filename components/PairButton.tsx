fe46d2a89b5d1bc67439176284fff2c8e715499b
bigandy5climate <snolly@protonmail.com>
2022-04-17 21:56:42 +0300

    fast refresh connect~ status

--- components/PairButton.tsx
+++ components/PairButton.tsx
@@ -1,12 +1,13 @@
 import { Client } from '@hashgraph/sdk'
+import { useState } from 'react'
 import {init, pair}from '../helpers/hashconnect'
 function genqr() {
     
 }
 const client = Client.forTestnet
-
 export default function () {
+    const [connected, setConnected]=useState(false)
     return (
-        <button style={{backgroundColor:'purple', borderRadius:10}} onClick={async ()=>{await init();pair()}}>🔗🎒{sessionStorage.getItem('connectetd')=='true'?'- 🟢':''}</button>
+        <button style={{backgroundColor:'purple', borderRadius:10}} onClick={async ()=>{await init();pair();sessionStorage.getItem('connectetd')=='true'?setConnected(true):null}}>🔗🎒{connected?'- 🟢':''}</button>
     )
 }
