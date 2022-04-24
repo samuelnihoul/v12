f0d90f311516700dcceb76900b73f0bc8609a78d
bigandy5climate <snolly@protonmail.com>
2022-04-24 18:47:52 +0300

    bckp

--- helpers/hashconnect.ts
+++ helpers/hashconnect.ts
@@ -1,54 +1,176 @@
-import {HashConnect, HashConnectTypes} from 'hashconnect'
+import { ButtonLayoutDisplay, ButtonMaker, DialogInitializer, DialogLayoutDisplay } from '@costlydeveloper/ngx-awesome-popup';
+import { Transaction, TransactionReceipt } from '@hashgraph/sdk';
+import { HashConnect, HashConnectTypes, MessageTypes } from 'hashconnect';
+import { useState } from 'react';
+import { ResultModalComponent } from '../components/result-modal/result-modal.component';
 
-let hashconnect: HashConnect;
+
+
+// !! this line is a duplicate and unsure what will be the effects
+    const hashconnect: HashConnect=new HashConnect(true);
+    const [status,setStatus]= useState( "Initializing");
+    const a:HashConnectTypes.WalletMetadata[]=new Array(0)
+    const [availableExtensions,sae] = useState(a)
+
+    const [saveData,ssd] /* {
+        topic: string;
+        pairingString: string;
+        privateKey?: string;
+        pairedWalletData?: HashConnectTypes.WalletMetadata;
+        pairedAccounts: string[];
+    }  */= useState({
+        topic: "",
+        pairingString: "",
+        privateKey: undefined,
+        pairedWalletData: undefined,
+        pairedAccounts: []
+    })
+
+     const appMetadata: HashConnectTypes.AppMetadata = {
+        name: "dApp Example",
+        description: "An example hedera dApp",
+        icon: "https://www.hashpack.app/img/logo.svg"
+    }
+
+    async function initHashconnect() {
+        //create the hashconnect instance
+        const hashconnect = new HashConnect(true);
+
+        if(!loadLocalData()){
+            //first init, store the private key in localstorage
+            let initData = await hashconnect.init(appMetadata);
+            saveData.privateKey = initData.privKey;
+
+            //then connect, storing the new topic in localstorage
+            const state = await hashconnect.connect();
+            console.log("Received state", state);
+            saveData.topic = state.topic;
+            
+            //generate a pairing string, which you can display and generate a QR code from
+            saveData.pairingString = hashconnect.generatePairingString(state, "testnet", true);
+            
+            //find any supported local wallets
+            hashconnect.findLocalWallets();
+
+            setStatus( "Connected");
+        }
+        else {
+            await hashconnect.init(appMetadata, saveData.privateKey);
+            await hashconnect.connect(saveData.topic, saveData.pairedWalletData!);
+
+setStatus('Paired');
+        }
+
+        setUpEvents();
+    }
+
+    function setUpEvents() {
+
+        hashconnect.foundExtensionEvent.on((data) => {
+            availableExtensions.push(data);
+            console.log("Found extension", data);
+        })
+
+
+        // hashconnect.additionalAccountResponseEvent.on((data) => {
+        //     console.log("Received account info", data);
+            
+        //     data.accountIds.forEach(id => {
+        //         if(saveData.pairedAccounts.indexOf(id) == -1)
+        //             saveData.pairedAccounts.push(id);
+        //     })
+        // })
+
+        hashconnect.pairingEvent.on((data) => {
+            console.log("Paired with wallet", data);
+            setStatus ("Paired");
+
+            saveData.pairedWalletData = data.metadata;
+
+            data.accountIds.forEach(id => {
+                if(saveData.pairedAccounts.indexOf(id) == -1)
+                    ssd(s=>{let t=s;t.pairedAccounts.push(id);return t});
+            })
+
+            saveDataInLocalstorage();
+        });
+
+
+        hashconnect.transactionEvent.on((data) => {
+            //this will not be common to be used in a dapp
+            console.log("transaction event callback");
+        });
+    }
+
+    async function connectToExtension() {
+        hashconnect.connectToLocalWallet(saveData.pairingString);
+    }
+
+
+    async function sendTransaction(trans: Uint8Array, acctToSign: string, return_trans: boolean = false) {
+        const transaction: MessageTypes.Transaction = {
+            topic: saveData.topic,
+            byteArray: trans,
+            
+            metadata: {
+                accountToSign: acctToSign,
+                returnTransaction: return_trans
+            }
+        }
+
+        return await hashconnect.sendTransaction(saveData.topic, transaction)
+    }
+
+    async function requestAccountInfo() {
+        let request:MessageTypes.AdditionalAccountRequest = {
+            topic: saveData.topic,
+            network: "mainnet",
+            multiAccount: true
+        } 
+
+        await hashconnect.requestAdditionalAccounts(saveData.topic, request);
+    }
+
+    function saveDataInLocalstorage() {
+        let data = JSON.stringify(saveData);
+        
+        localStorage.setItem("hashconnectData", data);
+    }
+
+    function loadLocalData() :boolean {
+        let foundData = localStorage.getItem("hashconnectData");
+
+        if(foundData){
+            ssd ( JSON.parse(foundData));
+            console.log("Found local data", saveData)
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function clearPairings() {
+        saveData.pairedAccounts = [];
+        saveData.pairedWalletData = undefined;
+        setStatus('Connected')
+        localStorage.removeItem("hashconnectData");
+    }
+
+    function showResultOverlay(data: any) {
+            const dialogPopup = new DialogInitializer(ResultModalComponent);
     
-let saveData = {
-    topic: "",
-    pairingString: "",
-    privateKey: "",
-    pairedWalletData: null,
-    pairedAccounts: []
-}
-
-let appMetadata: HashConnectTypes.AppMetadata = {
-    name: "dApp Example",
-    description: "An example hedera dApp",
-    icon: "https://www.hashpack.app/img/logo.svg"
-}
-
-async function initHashconnect() {
-    //create the hashconnect instance
-    hashconnect = new HashConnect();
-
-    if(!loadLocalData()){
-        //first init and store the private for later
-        let initData = await hashconnect.init(appMetadata);
-        saveData.privateKey = initData.privKey;
-
-        //then connect, storing the new topic for later
-        const state = await hashconnect.connect();
-        saveData.topic = state.topic;
-        
-        //generate a pairing string, which you can display and generate a QR code from
-        saveData.pairingString = hashconnect.generatePairingString(state, "testnet", true);
-        
-        //find any supported local wallets
-        hashconnect.findLocalWallets();
+            dialogPopup.setCustomData({ data: data });
+            
+            dialogPopup.setConfig({
+                width: '500px',
+                layoutType: DialogLayoutDisplay.NONE
+            });
+    
+            dialogPopup.setButtons([
+                new ButtonMaker('Done', 'send', ButtonLayoutDisplay.SUCCESS)
+            ]);
+    
+            dialogPopup.openDialog$().subscribe(resp => { });
     }
-    else {
-        //use loaded data for initialization + connection
-        await hashconnect.init(appMetadata, saveData.privateKey);
-        await hashconnect.connect(saveData.topic, saveData.pairedWalletData);
-    }
-}
 
-function loadLocalData(): boolean {
-    let foundData = localStorage.getItem("hashconnectData");
-
-    if(foundData){
-        saveData = JSON.parse(foundData);
-        return true;
-    }
-    else
-        return false;
-}
+    export {showResultOverlay,clearPairings,requestAccountInfo,sendTransaction,connectToExtension,initHashconnect,status}
