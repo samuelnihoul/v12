22abc87dee02d10b1abe3c67cd93325f350396d9
bigandy5climate <snolly@protonmail.com>
2022-04-24 15:45:06 +0300

    cp

--- helpers/hashconnect.ts
+++ helpers/hashconnect.ts
@@ -1,38 +1,54 @@
-import { HashConnect, HashConnectTypes } from 'hashconnect'
-import { allowedNodeEnvironmentFlags } from 'process';
-let hashconnect = new HashConnect
-let appMetadata: HashConnectTypes.AppMetadata = {
-    name: "Karbon Basar",
-    description: "The web3 carbon offset marketplace",
-    icon: "/assets/images/D21.png"
-}
-async function init() {
-    let initData
-    //maybe here it automatically becomes 'undefined' when queried, if non existinng
-    localStorage.getItem('toxen')?localStorage.getItem('toxen')!=='undefined'?initData=localStorage.getItem('toxen'):initData = await hashconnect.init(appMetadata):'';
-    //console.log('init')
-    let privateKey = initData.privKey; 
-    localStorage.setItem('toxen',privateKey)
-}
-async function connectToNodNPairingString() {
+import {HashConnect, HashConnectTypes} from 'hashconnect'
+
+let hashconnect: HashConnect;
     
-    let state = await hashconnect.connect();
-let topic
-    localStorage.getItem('topi') ? topic = state.topic + localStorage.setItem('topi', topic) : topic
-    return hashconnect.generatePairingString(state, "testnet", false);//tihs is the pairing string
-}
-// function findWallet() {
-//    return hashconnect.findLocalWallets()
-// }
-async function setConnected(a:boolean){
-    sessionStorage.setItem('connectetd',a?'true':'false')
-}
-async function pair() {
-    hashconnect.foundExtensionEvent.once((walletMetadata)=>{sessionStorage.setItem('theDudeHasHashpack','true')})
-    hashconnect.findLocalWallets()
-    sessionStorage.getItem('theDudeHasHashpack')? hashconnect.connectToLocalWallet(await connectToNodNPairingString().then((e)=>{setConnected(true);return e})):alert('Please install the Hashpack browser extension 😊')
-   
-    
+let saveData = {
+    topic: "",
+    pairingString: "",
+    privateKey: "",
+    pairedWalletData: null,
+    pairedAccounts: []
 }
 
-export {init, pair,connectToNodNPairingString}
+let appMetadata: HashConnectTypes.AppMetadata = {
+    name: "dApp Example",
+    description: "An example hedera dApp",
+    icon: "https://www.hashpack.app/img/logo.svg"
+}
+
+async function initHashconnect() {
+    //create the hashconnect instance
+    hashconnect = new HashConnect();
+
+    if(!loadLocalData()){
+        //first init and store the private for later
+        let initData = await hashconnect.init(appMetadata);
+        saveData.privateKey = initData.privKey;
+
+        //then connect, storing the new topic for later
+        const state = await hashconnect.connect();
+        saveData.topic = state.topic;
+        
+        //generate a pairing string, which you can display and generate a QR code from
+        saveData.pairingString = hashconnect.generatePairingString(state, "testnet", true);
+        
+        //find any supported local wallets
+        hashconnect.findLocalWallets();
+    }
+    else {
+        //use loaded data for initialization + connection
+        await hashconnect.init(appMetadata, saveData.privateKey);
+        await hashconnect.connect(saveData.topic, saveData.pairedWalletData);
+    }
+}
+
+function loadLocalData(): boolean {
+    let foundData = localStorage.getItem("hashconnectData");
+
+    if(foundData){
+        saveData = JSON.parse(foundData);
+        return true;
+    }
+    else
+        return false;
+}
