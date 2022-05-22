7bb7b526cca70597d27a810969dd0fb96155c15b
samuelnihoul <contact@harmonia-eko.oooo>
2022-05-22 09:01:27 +0000

    Revert "Revert "fixed google analytics class""
    This reverts commit 9ccc1bab08ca89875be97202471371800ccb9f53.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { Component, useEffect } from "react";
 import '../css/style.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../public/icofont/icofont.min.css'
@@ -6,13 +6,18 @@ import HeaderOne from "../components/Header/HeaderOne";
 import FooterOne from "../components/Footer/FooterOne";
 import Sound from 'react-sound'
 import ReactGA from "react-ga";
+import App from "next/app" ;
 
 
-export default function App({ Component, pageProps }) {
-  useEffect(()=>{
+export default class MyApp extends App {
+  componentDidMount() {
     ReactGA.initialize("G-RFBEGZHSQT");
     ReactGA.pageview(window.location.pathname + window.location.search);
-  },[])
-  return (<>
-  <Component className="App" {...pageProps} /></>)
+  }
+  // return (<>
+  // <Component className="App" {...pageProps} /></>)
+  render(){
+    return (<>
+     <Component className="App" {...this.props} /></>)
+  }
 }
