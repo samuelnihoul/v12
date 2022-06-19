7dc7ef49c7d341486ba3a6024f138c8de2c23217
samuelnihoul <contact@harmonia-eko.oooo>
2022-06-19 14:24:01 +0000

    button !working

--- components/WelcomeSection/Welcome.tsx
+++ components/WelcomeSection/Welcome.tsx
@@ -3,7 +3,7 @@ import { JsxChild } from 'typescript';
 import HeadingSection from '../HeadingSection/HeadingSection';
 const Welcome = ({ title, tagline, children }) => (
   <section className="first-ico-box" id="about" style={{textAlign:'center'}}>
-    <h6 style={{textAlign: "center"}}>NFT Collection Q1 2022 - Display yours on your digital handles to spread your ethics!</h6>
+    <h6 style={{textAlign: "center"}}>NFT Collection #1 - Display yours on your digital handles to spread your ethics!</h6>
     <img style={{paddingBottom: "3vh"}} src={"/assets/images/image17.png"}alt="NFTs">
               </img>
     
